b879e5cb66cab204814441f6be45a042098d0e6a
Philip Catullo <philip.catullo@revature.net>
2021-12-03 16:39:37 -0500

    Weekly Coding Challenge RPS

---
+++ RPS.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class RPS
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+
+            Console.WriteLine("Player 1: Please enter your choice.");
+
+            string player1 = Console.ReadLine();
+
+            Console.WriteLine("Thank you, Player 1");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Player 2: Please enter your choice.");
+
+            string player2 = Console.ReadLine();
+
+            Console.WriteLine("Thank you, Player 2");
+
+            Console.WriteLine("");
+
+            playRPS(player1, player2);
+        }
+
+        public static void playRPS(string player1, string player2)
+        {
+
+            if (player1 == player2)
+            {
+                Console.WriteLine("Tie!");
+            }
+            else if (player1 == "Paper" && player2 == "Rock")
+            {
+                Console.WriteLine("Player 1 Wins!");
+            }
+            else if (player1 == "Rock" && player2 == "Scissors")
+            {
+                Console.WriteLine("Player 1 Wins!");
+            }
+            else if (player1 == "Scissors" && player2 == "Paper")
+            {
+                Console.WriteLine("Player 1 Wins!");
+            }
+            else if (player2 == "Paper" && player1 == "Rock")
+            {
+                Console.WriteLine("Player 2 Wins!");
+            }
+            else if (player2 == "Rock" && player1 == "Scissors")
+            {
+                Console.WriteLine("Player 2 Wins!");
+            }
+            else if (player2 == "Scissors" && player1 == "Paper")
+            {
+                Console.WriteLine("Player 2 Wins!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Input. Please choose either Rock, Paper. or Scissors");
+            }
+        }
+    }
+}
